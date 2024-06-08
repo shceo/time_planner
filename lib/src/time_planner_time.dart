b81ca15805ba70a8f8268d1ad24a2d6c29912949
shceo <fayzullojalolov42@gmail.com>
2024-06-08 18:00:26 +0500

    fixed

--- lib/src/time_planner_time.dart
+++ lib/src/time_planner_time.dart
@@ -23,11 +23,11 @@ class TimePlannerTime extends StatelessWidget {
         padding: const EdgeInsets.only(top: 0, bottom: 12),
         child: setTimeOnAxis!
             ? Transform.translate(
-                offset: const Offset(0, -11),
+                offset: const Offset(0, -10),
                 child: Text(time!),
               )
             : Transform.translate(
-                offset: const Offset(0, -11),
+                offset: const Offset(0, -10),
                 child: Text(time!),
               ),
       ),
